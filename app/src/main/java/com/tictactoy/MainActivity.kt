e9bcf2fd7a458547f56456f50f4a8a1cbff1fecb
kenjimaeda54 <kenjimaedafamily@gmail.com>
2022-02-20 18:13:53 -0300

    feat: finished logic button set listener

--- app/src/main/java/com/tictactoy/MainActivity.kt
+++ app/src/main/java/com/tictactoy/MainActivity.kt
@@ -2,10 +2,41 @@ package com.tictactoy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (supportActionBar != null) {
+            supportActionBar!!.hide()
+        }
+        buttonOne.setOnClickListener(this)
+        buttonTwo.setOnClickListener(this)
+        buttonThree.setOnClickListener(this)
+        buttonFour.setOnClickListener(this)
+        buttonFive.setOnClickListener(this)
+        buttonSix.setOnClickListener(this)
+        buttonSeven.setOnClickListener(this)
+        buttonEight.setOnClickListener(this)
+        buttonNine.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        var idClick = view.id;
+        val numberClicked = when (idClick) {
+            R.id.buttonOne -> 1
+            R.id.buttonTwo -> 2
+            R.id.buttonThree -> 3
+            R.id.buttonFour -> 4
+            R.id.buttonFive -> 5
+            R.id.buttonSix -> 6
+            R.id.buttonSeven -> 7
+            R.id.buttonEight -> 8
+            else -> 9
+        }
+        Log.d("clicked",numberClicked.toString())
     }
 }
